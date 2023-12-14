76b4358da20c92657a08106a2ac9078f0246bb4f
Sweekt <benjamin.roy69@gmail.com>
2023-12-14 23:35:55 +0100

    Checker updated

--- srcs/map_solve_check.c
+++ srcs/map_solve_check.c
@@ -6,11 +6,43 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 17:04:31 by beroy             #+#    #+#             */
-/*   Updated: 2023/12/14 18:21:10 by beroy            ###   ########.fr       */
+/*   Updated: 2023/12/14 18:40:53 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+int fill_checker(char **map)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'C' || map[i][j] == 'E')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	map_filler(char **map, size_t x, size_t y)
+{
+	if (map[x][y] != '1')
+	{
+		map[x][y] = 'O';
+		map_filler(map, x - 1, y);
+		map_filler(map, x + 1, y);
+		map_filler(map, x, y - 1);
+		map_filler(map, x, y + 1);
+	}
+}
 
 int path_checker(char** map, size_t x, size_t y)
 {
@@ -19,8 +51,10 @@ int path_checker(char** map, size_t x, size_t y)
 	map_clone = ft_tabdup(map);
 	if (map_clone == NULL)
 		return (1);
-	map_filler(map_clone);
-
+	map_filler(map_clone, x, y);
+	if (fill_checker(map_clone) == 1)
+		return (1);
+	return (0);
 }
 
 int	solver_check(char **map)
